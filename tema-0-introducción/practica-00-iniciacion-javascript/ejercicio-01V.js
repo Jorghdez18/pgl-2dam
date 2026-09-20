d55a52a8de595f318097497f6eee3a253b209dd4
Jorge Hernández Pérez <jorcarherpe@gmail.com>
2026-09-20 11:49:40 +0100

    Add files via upload

---
+++ tema-0-introducción/practica-00-iniciacion-javascript/ejercicio-01V.js
@@ -0,0 +1,10 @@
+//console.log("Hola, JavaScript");
+
+//console.log("Estoy preparando mi primera Pokédex");
+//console.log("Durante el curso utilizaremos JavaScript y TypeScript");
+
+const nombre = "Dragonite"; 
+const tipo = "dragón";
+const nivel = 76;
+
+console.log(`${nombre} es de tipo ${tipo} y tiene nivel ${nivel}.`); //He decidido mostrar directamente el mensaje combinandolo con las variables cambiadas mencioanadas en el ejercicio.
